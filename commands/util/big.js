90c350e6e965a2757cfd71de2ef57fc8fad06dce
Adrian <88adrianlee@gmail.com>
2021-06-05 00:20:44 -0700

    add more and bigger emotes

---
+++ commands/util/big.js
@@ -0,0 +1,30 @@
+const config = require('../../config.json');
+const betterTwitchScraper = require('../../scraper/bttv-scraper');
+const twitchScraper = require('../../scraper/ttv-scraper');
+const axios = require('axios');
+
+
+module.exports = {
+	name: 'big',
+	description: 'this is a big emote search command!',
+	async execute(message, args) {
+		for (let i = 0; i < args.length; i++) {
+			let imageURL = betterTwitchScraper.findBetterTwitchEmoteBig(args[i]);
+			if (imageURL === config.NO_EMOTE_FOUND) {
+				imageURL = await twitchScraper.findEmote(args[i]);
+				if (imageURL === config.NO_EMOTE_FOUND) {
+					message.channel.send(`${message.author.username}, there is no emote with name ${args[i]}.`);
+				}
+				else {
+					message.channel.send(message.author.username + ':');
+					message.channel.send(imageURL);
+				}
+			}
+			else {
+				const response = await axios.get(imageURL, { responseType: 'arraybuffer' });
+				const buffer = Buffer.from(response.data, 'utf-8');
+				message.channel.send(message.author.username + ':', { files: [imageURL] });
+			}
+		}
+	},
+};
--- scraper/bttv-scraper.js
+++ scraper/bttv-scraper.js
@@ -1,45 +1,54 @@
 const axios = require('axios');
 const fs = require('fs');
-const cheerio = require('cheerio');
 
 const config = require('../config.json');
-const { getSiteHtml } = require('../scraper/scraper');
 const betterEmotes = require('../bttvemotes.json');
 
-const offsetLimit = 2000;
+const offsetLimit = 10000;
 let emotes = [];
 
 const fsPromises = fs.promises;
 class betterTwitchScraper {
-    async getBetterTwitchEmotes() {
-        for (let offset = 0; offset <= offsetLimit; offset += 50) {
-            let response = await axios.get(`https://api.betterttv.net/3/emotes/shared/top?offset=${offset}&limit=50`)
-            let currentEmotes = response.data.map((emoteData) => ({
-                emoteName: emoteData.emote.code,
-                emoteID: emoteData.emote.id
-            }));
-            emotes = emotes.concat(currentEmotes);
-        }
+	async getBetterTwitchEmotes() {
+		for (let offset = 0; offset <= offsetLimit; offset += 50) {
+			const response = await axios.get(`https://api.betterttv.net/3/emotes/shared/top?offset=${offset}&limit=50`);
+			const currentEmotes = response.data.map((emoteData) => ({
+				emoteName: emoteData.emote.code,
+				emoteID: emoteData.emote.id,
+			}));
+			emotes = emotes.concat(currentEmotes);
+		}
 
-        try {
-            await fsPromises.writeFile(config.BTTV_EMOTES_PATH, JSON.stringify(emotes, null, 2),);
-        }
-        catch (err) {
-            console.log(err);
-        }
-        // return emotes;
-    }
+		try {
+			await fsPromises.writeFile(config.BTTV_EMOTES_PATH, JSON.stringify(emotes, null, 2));
+		}
+		catch (err) {
+			console.log(err);
+		}
+		// return emotes;
+	}
 
-    findBetterTwitchEmote(emote) {
-        for(let i = 0; i < betterEmotes.length; i++) {
-            if(betterEmotes[i]['emoteName'].toLowerCase() === emote.toLowerCase()) {
-                let imageID = (betterEmotes[i]['emoteID']);
-                return `https://cdn.betterttv.net/emote/${imageID}/1x.gif`;
-            }
-        }
-        return config.NO_EMOTE_FOUND;
+	findBetterTwitchEmote(emote) {
+		for (let i = 0; i < betterEmotes.length; i++) {
+			if (betterEmotes[i]['emoteName'].toLowerCase() === emote.toLowerCase()) {
+				const imageID = (betterEmotes[i]['emoteID']);
+				return `https://cdn.betterttv.net/emote/${imageID}/1x.gif`;
+			}
+		}
+		return config.NO_EMOTE_FOUND;
 
-    }
+	}
+
+    findBetterTwitchEmoteBig(emote) {
+		for (let i = 0; i < betterEmotes.length; i++) {
+			if (betterEmotes[i]['emoteName'].toLowerCase() === emote.toLowerCase()) {
+				const imageID = (betterEmotes[i]['emoteID']);
+				return `https://cdn.betterttv.net/emote/${imageID}/3x.gif`;
+			}
+		}
+		return config.NO_EMOTE_FOUND;
+
+	}
 }
 
 module.exports = new betterTwitchScraper();
